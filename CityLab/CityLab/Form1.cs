289eb0cc58718a52cfd8d18af65a1f851d081381
dartslam <dart.slam@mail.ru>
2014-04-05 18:55:21 +0700

    Main Form & add workable timer

---
+++ CityLab/CityLab/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CityLab
+{
+    public partial class Form1 : Form
+    {
+
+        int timerCounter = 60;                     
+        System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer.Interval = 1000;
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label2.Hide();
+            textBox2.Hide();
+            label3.Hide();
+            textBox3.Hide();
+            label1.Hide();
+            textBox1.Hide();
+            button1.Hide();
+        }
+
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            //В текстбокс выводим значение timerCounter уменьшаемое на 1
+            this.textBox3.Text = (--timerCounter).ToString();
+            if (timerCounter == 0)
+            {
+                timer.Stop();
+                MessageBox.Show("Время истекло! \nВы проиграли!");
+                
+                this.Close();
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            timer = new Timer();
+            timer.Interval = 1000;
+            timerCounter = 60;
+            timer.Tick += new EventHandler(timer1_Tick_1); //подписываемся на события Tick
+            timer.Start();
+            label2.Show();
+            textBox2.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timerCounter = 60;
+            button3.Hide();
+            timer.Tick += new EventHandler(timer1_Tick_1); //подписываемся на события Tick
+            timer.Start();
+            label3.Show();
+            textBox3.Show();
+            label1.Show();
+            textBox1.Show();
+            button1.Show();
+        }
+    }
+}
---
+++ CityLab/CityLab/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("CityLab")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CityLab")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("279b36dc-4784-4374-87fa-d9e9a2663848")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер построения
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
